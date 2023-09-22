3bedd45ad69497ce10d0b2b2945b9689bae0f7e5
patricio <patricio.16campos@gmail.com>
2023-09-22 11:20:06 -0300

    termino de rama1

--- PECY_app1/views.py
+++ PECY_app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def  display1(request):
+    return HttpResponse("<h1>hola vista 1 </h1>")
+
+def display2(request):
+    return HttpResponse("<h2>hola vista 2 </h1>")
--- PECY_evaluacion1/settings.py
+++ PECY_evaluacion1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PECY_app1',
+
 ]
 
 MIDDLEWARE = [
