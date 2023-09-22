14bcead49dc8e80ea5622fcf352304b81cbe814a
patricio <patricio.16campos@gmail.com>
2023-09-22 11:39:53 -0300

    arreglando error

--- PECY_app2/views.py
+++ PECY_app2/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def display3(request):
-    return HtppResponse("<h3> hola vista 3</h3>")
+    return HttpResponse("<h3> hola vista 3</h3>")
 
 def display4(request):
     return HttpResponse("<h4> hola vista 4</h4>")
